1e38e28de9f68c6a7131fdcc08872579b4ed8561
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-11-21 14:42:05 +0600

    feature(all): base_js module created + 01 module solved

---
+++ js_by_vladilen_base_js/01/task_03/js-info.js
@@ -0,0 +1,18 @@
+// кто разработал язык программирования javascript
+alert("Брендан Эйх");
+
+
+// за сколько дней был разработан javascript
+alert("JavaScript был создан за 10 дней");
+
+
+// в каком году появился javascript
+alert("Javascript появился в сентябре 1995 года");
+
+
+// JavaScript Это тоже самое что и Java?
+alert("JS это не Java")
+
+
+// как называется спецификация javascript
+alert("ECMAScript")
---
+++ js_by_vladilen_base_js/01/task_04_prompt/index.js
@@ -0,0 +1,22 @@
+let sum = prompt("Сколько будет 2 + 2");
+
+if (sum === "4")
+    alert("Верно");
+else
+    alert("Неверно");
+
+
+let sqrt = prompt("Чему равен корень из 9");
+
+if (sqrt === "3")
+    alert("Верно");
+else
+    alert("Неверно");
+
+
+let power = prompt("Сколько будет 2 в 5-й степени");
+
+if (power === "32")
+    alert("Верно");
+else
+    alert("Неверно");
