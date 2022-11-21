44420e6e98532629da0cae48ec13f7837b830335
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-11-21 18:04:42 +0600

    feature(all): module 3

---
+++ js_by_vladilen_base_js/03/task_01/data_types.js
@@ -0,0 +1,15 @@
+const obj = null;
+const undef = undefined;
+const bool = true;
+const num = 25;
+const bigInt = 25n;
+const str = "String";
+const symb = Symbol("symb");
+
+console.log("obj", obj, typeof obj);
+console.log("undef", undef, typeof undef);
+console.log("bool", bool, typeof bool);
+console.log("num", num, typeof num);
+console.log("bigInt", bigInt, typeof bigInt);
+console.log("str", str, typeof str);
+console.log("symb", symb, typeof symb);
---
+++ js_by_vladilen_base_js/03/task_02/index.js
@@ -0,0 +1,11 @@
+let num = 20;
+let str = "string";
+let bool = true;
+
+num = "stirng";
+str = 20;
+bool = null;
+
+console.log(num);
+console.log(str);
+console.log(bool);
