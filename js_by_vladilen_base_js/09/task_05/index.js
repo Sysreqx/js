a38d028a999b0323daa771df5fb8f48388af46a0
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-11-23 12:12:44 +0600

    feature(all): module 9, task 5

---
+++ js_by_vladilen_base_js/09/task_05/index.js
@@ -0,0 +1,22 @@
+const coffees = ['Latte', 'Cappuccino', 'Americano'];
+const prices = [1.5, 1, 2];
+let updatedPrices = prices.map((price) => {
+    return price *= 1.5;
+});
+
+let clientEstimations = [];
+
+const askClientToGiveEstimation = () => {
+    let n = Number.parseInt(prompt("Как вы оцениваете нашу кофейню от 1 до 10"));
+
+    if (n >= 1 && n <= 10) {
+        clientEstimations.push(n);
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    askClientToGiveEstimation();
+}
+
+alert(`Всего положительных оценок: ${clientEstimations.filter(e => e <= 5).length};
+Всего отрицательных оценок: ${clientEstimations.filter(e => e > 5).length}`);
