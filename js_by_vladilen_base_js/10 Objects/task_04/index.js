aed4ead55d88a76468afb859132b1b63fb35b94d
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-11-23 16:12:16 +0600

    feature(all): module 10, task 4

---
+++ js_by_vladilen_base_js/10 Objects/task_04/index.js
@@ -0,0 +1,25 @@
+const giveJobToStudent = (student, jobTitle) => {
+  return {
+      ...student,
+      job: jobTitle
+  };
+};
+
+const student = {
+    fullName: 'Максим',
+    experienceInMonths: 12,
+    stack: ['HTML', 'CSS', 'JavaScript', 'React'],
+}
+
+const updatedStudent = giveJobToStudent(student, 'веб-разработчик');
+
+console.log(updatedStudent);
+
+/*
+updatedStudent = {
+fullName: 'Максим',
+experienceInMonths: 12,
+stack: ['HTML', 'CSS', 'JavaScript', 'React'],
+job: 'веб-разработчик',
+}
+ */
