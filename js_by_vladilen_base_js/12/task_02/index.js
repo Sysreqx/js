a151de111806256b22e2da3070ffed5956686a90
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-11-24 13:43:38 +0600

    feature(all): module 12, task 2

---
+++ js_by_vladilen_base_js/12/task_02/index.js
@@ -0,0 +1,27 @@
+const dog = {
+    name: 'Чарли',
+    type: 'Собака',
+    makeSound() {
+        return 'Гав-Гав';
+    }
+}
+
+const bird = {
+    name: 'Петя',
+    type: 'Воробей',
+    makeSound() {
+        return 'Чик-чирик';
+    }
+}
+
+function makeDomestic(isDomestic) {
+    console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`);
+
+    this["isDomestic"] = isDomestic;
+
+    return this;
+}
+
+console.log(makeDomestic.bind(dog, true)());
+console.log(makeDomestic.apply(bird, [false]));
+console.log(makeDomestic.call(bird, true));
