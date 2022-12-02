704dbd18cf4da071b1ea31af7114e6ec0fd39506
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-12-02 11:01:07 +0600

    feature(all): module 14, task 1

--- js_by_vladilen_base_js/14 Working with DOM/task_01/index.js
+++ js_by_vladilen_base_js/14 Working with DOM/task_01/index.js
@@ -1,3 +1,19 @@
+/////////////
+document.body.innerHTML = `
+    <form class="create-user-form">
+        <label>
+          Имя
+          <input type="text" name="userName" placeholder="Введите ваше имя">
+        </label>
+        <label>
+          Пароль
+          <input type="password" name="password" placeholder="Придумайте пароль">
+        </label>
+        <button type="submit">
+          Подтвердить
+        </button>
+    </form>`;
+
 let createUserForm = document.createElement("form");
 createUserForm.className = "create-user-form";
 
@@ -10,12 +26,19 @@ input1.placeholder = "Введите Ваше имя";
 label1.append(input1);
 
 let label2 = document.createElement("label");
-label2.innerText = "Пароль ";
+label2.innerText = " Пароль ";
 let input2 = document.createElement("input");
 input2.type = "password";
 input2.name = "password";
 input2.placeholder = "Придумайте пароль";
+label2.append(input2);
 
+let btn = document.createElement("button");
+btn.innerText = "Подтвердить";
 
 createUserForm.append(label1);
+createUserForm.append(label2);
+createUserForm.append(btn);
 document.body.append(createUserForm);
+
+
