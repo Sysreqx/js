5baa3feafa853bec414ad964b9adced231305d10
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-12-28 19:30:00 +0600

    feature(all): trying some code

--- js_blocks_of_code/yandex eda/test.js
+++ js_blocks_of_code/yandex eda/test.js
@@ -2,16 +2,17 @@ let createButton = Array.from(document.querySelectorAll('.x6s0dn4')).find(el => 
 createButton.click();
 let dropArea = document.querySelector('._ac2r');
 
-file = "https://esquire.kz/wp-content/uploads/2022/12/2-2.jpg"
+let img = new Image(100, 200);
+img.src = 'https://esquire.kz/wp-content/uploads/2022/12/2-2.jpg';
 
-function previewFile(file) {
-    let reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onloadend = function() {
-        let img = document.createElement('img')
-        img.src = reader.result
 
-        dropArea.drop(img);
-    }
-}
+dropArea.appendChild(img);
 
+
+let comment_block = document.querySelector('textarea');
+console.log(comment_block);
+comment_block.classList.add("focus-visible");
+comment_block.setAttribute("data-focus-visible-added", "");
+comment_block.innerHTML = "text from bot";
+let clickBtn = document.querySelectorAll(".x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x1lku1pv.x1a2a7pz.xjyslct.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1i0vuye.xwhw2v2.x17ydfre.x1f6kntn.x2b8uid.xlyipyv.x87ps6o.x14atkfc.x1d5wrs8.x6s0dn4.xl56j7k.xt7dq6l.x972fbf.xcfux6l.x1qhh985.xm0m39n.xm3z3ea.x1x8b98j.x131883w.x16mih1h.xt0psk2.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.xjbqb8w.x1n5bzlp.x173jzuc.x1yc6y37");
+clickBtn.click();
