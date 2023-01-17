7f191b6b395074beea6bfe94923ef01b10aea92e
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-01-17 11:20:08 +0600

    feature(all): module 17 all tasks

---
+++ js_by_vladilen_base_js/17 Callback + Event Loop/task_01/index.js
@@ -0,0 +1,16 @@
+setTimeout(() => {
+    console.log('setTimeout');
+}, 0);
+const promise = new Promise((resolve) => {
+    console.log('Promise');
+    resolve();
+});
+promise.then(() => {
+    console.log('Promise resolve');
+});
+console.log('End');
+
+// Promise
+// end
+// Promise resolve
+// setTimeout
