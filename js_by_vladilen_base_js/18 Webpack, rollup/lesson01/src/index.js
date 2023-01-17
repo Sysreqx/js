3c8a8a67b41914c16bf047fbf775b25c82bf038a
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-01-17 13:39:54 +0600

    feature(all): module 18. plugin-babel (backwards-compatible JavaScript code that can be run by older JavaScript engines); plugin-styles = for css; plugin-img; plugin-serve + plugin-livereload (for localhost livereloading);
    in package.json (scripts for dev. and for prod.);

--- js_by_vladilen_base_js/18 Webpack, rollup/lesson01/src/index.js
+++ js_by_vladilen_base_js/18 Webpack, rollup/lesson01/src/index.js
@@ -1,1 +1,10 @@
+import '../index.css'
+import img from '../assets/js.png';
+
 console.log("Hello world");
+
+const jsImageHtml = document.createElement("img");
+jsImageHtml.className = "js-image";
+jsImageHtml.src = img;
+
+document.body.appendChild(jsImageHtml);
