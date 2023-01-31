8987b6df5ccca6a94cdf2a290e41f9831eab64ed
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-01-31 13:02:36 +0600

    feature(funsun): active map code

--- js_blocks_of_code/funsun_visitdubai/index.js
+++ js_blocks_of_code/funsun_visitdubai/index.js
@@ -1,14 +1,149 @@
 let map = document.querySelector(".map");
-let mapPhotos = document.querySelectorAll(".map__photo");
-let mapIcons = document.querySelectorAll(".map__icon");
+let mapPhotos = document.querySelectorAll(".map_photo");
+
+let mapPhotos1 = document.querySelectorAll(".map_photo_1");
+let mapPhoto1_1 = document.querySelector(".map_photo_1_1");
+let mapPhoto1_3 = document.querySelector(".map_photo_1_3");
+let mapPhoto1_5 = document.querySelector(".map_photo_1_5");
+let mapPhoto1_7 = document.querySelector(".map_photo_1_7");
+let mapPhoto1_8 = document.querySelector(".map_photo_1_8");
+
+let mapPhotos2 = document.querySelectorAll(".map_photo_2");
+let mapPhoto2_1 = document.querySelector(".map_photo_2_1");
+let mapPhoto2_2 = document.querySelector(".map_photo_2_2");
+let mapPhoto2_3 = document.querySelector(".map_photo_2_3");
+let mapPhoto2_4 = document.querySelector(".map_photo_2_4");
+
+let mapPhotos3 = document.querySelectorAll(".map_photo_3");
+let mapPhoto3_1 = document.querySelector(".map_photo_3_1");
+let mapPhoto3_3 = document.querySelector(".map_photo_3_3");
+let mapPhoto3_4 = document.querySelector(".map_photo_3_4");
+let mapPhoto3_5 = document.querySelector(".map_photo_3_5");
+let mapPhoto3_6 = document.querySelector(".map_photo_3_6");
+let mapPhoto3_7 = document.querySelector(".map_photo_3_7");
+
+let mapIcons = document.querySelectorAll(".map_icon");
+
+let mapLines = document.querySelectorAll(".map_line");
+let mapLine1 = document.querySelector(".map_line_1");
+let mapLine2 = document.querySelector(".map_line_2");
+let mapLine3 = document.querySelector(".map_line_3");
+
+const addTransparencyToAll = () => {
+    mapPhotos.forEach(el => {
+        el.classList.add("transparent-obj");
+    });
+    mapLines.forEach(el => {
+        el.classList.add("transparent-obj");
+    });
+}
+
+const removeTransparencyFromAll = () => {
+    mapPhotos.forEach(el => {
+        el.classList.remove("transparent-obj");
+    });
+    mapLines.forEach(el => {
+        el.classList.remove("transparent-obj");
+    });
+}
 
 map.addEventListener("mouseover", (event) => {
-   let {target} = event;
+    let {target} = event;
 
-   if (target.classList.contains("map__photo")) {
-       target.classList.opacity = "0.3";
-       console.log("3");
-   }
+    // console.log(target);
+    // transparent-obj
+
+    if (target.classList.contains("map_photo_1")) {
+
+        addTransparencyToAll();
+
+        mapPhotos1.forEach(el => {
+            el.classList.remove("transparent-obj");
+        })
+        mapLine1.classList.remove("transparent-obj");
+
+        // 1, 3, 5, 7, 8
+
+        // icon
+        // if (target.classList.contains("map_photo_1_1")) {
+        //     mapPhotos1.forEach(el => {
+        //         el.classList.remove("transparent-obj");
+        //     })
+        //     mapLine1.classList.remove("transparent-obj");
+        //
+        // } else if (target.classList.contains("map_photo_1_3")) {
+        //     mapPhoto1_3.classList.remove("transparent-obj");
+        // } else if (target.classList.contains("map_photo_1_5")) {
+        //     mapPhoto1_5.classList.remove("transparent-obj");
+        // } else if (target.classList.contains("map_photo_1_7")) {
+        //     mapPhoto1_7.classList.remove("transparent-obj");
+        // } else if (target.classList.contains("map_photo_1_8")) {
+        //     mapPhoto1_8.classList.remove("transparent-obj");
+        // }
+
+
+    } else if (target.classList.contains("map_photo_2")) {
+
+
+        addTransparencyToAll();
+
+        mapPhotos2.forEach(el => {
+                    el.classList.remove("transparent-obj");
+        })
+        mapLine2.classList.remove("transparent-obj");
+
+        // 1 ... 4
+
+        // icon
+        // if (target.classList.contains("map_photo_2_4")) {
+        //     mapPhotos2.forEach(el => {
+        //         el.classList.remove("transparent-obj");
+        //     })
+        //     mapLine2.classList.remove("transparent-obj");
+        //
+        // } else if (target.classList.contains("map_photo_2_1")) {
+        //     mapPhoto2_1.classList.remove("transparent-obj");
+        // } else if (target.classList.contains("map_photo_2_2")) {
+        //     mapPhoto2_2.classList.remove("transparent-obj");
+        // } else if (target.classList.contains("map_photo_2_3")) {
+        //     mapPhoto2_3.classList.remove("transparent-obj");
+        // }
+
+
+    } else if (target.classList.contains("map_photo_3")) {
+
+
+        addTransparencyToAll();
+
+        mapPhotos3.forEach(el => {
+                    el.classList.remove("transparent-obj");
+        })
+        mapLine3.classList.remove("transparent-obj");
+
+        // 1, 3 ... 7
+
+        // icon
+        // if (target.classList.contains("map_photo_3_7")) {
+        //     mapPhotos3.forEach(el => {
+        //         el.classList.remove("transparent-obj");
+        //     })
+        //     mapLine3.classList.remove("transparent-obj");
+        //
+        // } else if (target.classList.contains("map_photo_3_1")) {
+        //     mapPhoto3_1.classList.remove("transparent-obj");
+        // } else if (target.classList.contains("map_photo_3_3")) {
+        //     mapPhoto3_3.classList.remove("transparent-obj");
+        // } else if (target.classList.contains("map_photo_3_4")) {
+        //     mapPhoto3_4.classList.remove("transparent-obj");
+        // } else if (target.classList.contains("map_photo_3_5")) {
+        //     mapPhoto3_5.classList.remove("transparent-obj");
+        // } else if (target.classList.contains("map_photo_3_6")) {
+        //     mapPhoto3_6.classList.remove("transparent-obj");
+        // }
+
+
+    } else {
+        removeTransparencyFromAll();
+    }
+
 });
-
-
