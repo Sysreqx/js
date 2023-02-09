9165e65413ba8477a87edd7ce5edb1a7551f5cad
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-02-09 16:58:14 +0600

    feature(04): passing props to a component

--- js_by_vladilen_react_crash_course/01_introduction_to_react/src/components/counter.jsx
+++ js_by_vladilen_react_crash_course/01_introduction_to_react/src/components/counter.jsx
@@ -25,6 +25,7 @@ const Counter = (props) => {
 
     return (
         <div>
+            <span>{props.name}</span>
             <span className={getBadgeClasses()}>{formatCount()}</span>
             <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
                 +
--- js_by_vladilen_react_crash_course/01_introduction_to_react/src/components/countersList.jsx
+++ js_by_vladilen_react_crash_course/01_introduction_to_react/src/components/countersList.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import Counter from "./counter";
 
 const CountersList = () => {
-  let counters = [{id: 0, value: 0}, {id: 0, value: 3}, {id: 0, value: 0}];
+  let counters = [
+    { id: 0, value: 0, name: "Вещь" },
+    { id: 1, value: 3, name: "Ложка" },
+    { id: 2, value: 0, name: "Вилка" },
+    { id: 3, value: 0, name: "Тарелка" },
+    { id: 4, value: 1, name: "Набор бичугана" },
+  ];
   return (
     <>
       {counters.map((c) => (
-        <Counter key={c.id} value={c.value} />
+        <Counter key={c.id} value={c.value} name={c.name} />
       ))}
     </>
   );
