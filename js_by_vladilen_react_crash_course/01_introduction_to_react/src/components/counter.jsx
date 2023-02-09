83417cfee3b40d26915310817baeed3712ad0691
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-02-09 10:48:13 +0600

    feature(01_introduction_to_react): passing event arguments

--- js_by_vladilen_react_crash_course/01_introduction_to_react/src/components/counter.jsx
+++ js_by_vladilen_react_crash_course/01_introduction_to_react/src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
     const formatCount = () => {
         return count === 0 ? "empty" : count;
@@ -23,8 +24,24 @@ const Counter = () => {
         console.log(count);
     };
 
+    const handleTagChange = (id) => {
+        setTags((prevState) => prevState.filter((tag) => tag !== id));
+        //   console.log(id);
+    };
+
     return (
         <>
+            <ul>
+                {tags.map((tag) => (
+                    <li
+                        key={tag}
+                        className="btn btn-primary btn-sm m-2"
+                        onClick={() => handleTagChange(tag)}
+                    >
+                        {tag}
+                    </li>
+                ))}
+            </ul>
             <span className={getBadgeClasses()}>{formatCount()}</span>
             <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
                 +
