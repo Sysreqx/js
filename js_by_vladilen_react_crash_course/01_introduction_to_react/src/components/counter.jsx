0fbf2111be185555f64608d14f20d06d82b5c742
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-02-09 10:01:10 +0600

    feature(01_introduction_to_react): Event handling

--- js_by_vladilen_react_crash_course/01_introduction_to_react/src/components/counter.jsx
+++ js_by_vladilen_react_crash_course/01_introduction_to_react/src/components/counter.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Counter = () => {
-    const count = 2;
+    const [count, setCount] = useState(0);
 
     const formatCount = () => {
         return count === 0 ? "empty" : count;
     };
 
+    const getBadgeClasses = () => {
+        let classes = "badge m-2 ";
+        classes += count === 0 ? "bg-warning" : "bg-primary";
+        return classes;
+    };
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+        console.log(count);
+    };
+
+    const handleDecrement = () => {
+        setCount(count - 1);
+        console.log(count);
+    };
+
     return (
         <>
-            <span className="badge bg-primary m-2">{formatCount()}</span>
-            <button className="btn btn-primary btn-sm m-2">+</button>
+            <span className={getBadgeClasses()}>{formatCount()}</span>
+            <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+                +
+            </button>
+            <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
+                -
+            </button>
         </>
     );
 };
