0d435599a7ae90f70639123f61f1229baf5cf433
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-02-09 15:20:53 +0600

    feature(03_course_project): complete

--- js_by_vladilen_react_crash_course/03_course_project/src/components/user.jsx
+++ js_by_vladilen_react_crash_course/03_course_project/src/components/user.jsx
@@ -1,9 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import api from "../api";
 
 const Users = () => {
-  console.log(api.users.fetchAll());
-  return <h1>Users</h1>;
+  const [users, setUsers] = useState(api.users.fetchAll());
+  // console.log(users);
+
+  const handleDelete = (userId) => {
+    setUsers(users.filter((u) => u._id !== userId));
+    // console.log(userId);
+  };
+
+  const renderPhrase = (number) => {
+    if (users.length === 0) return "No users";
+    return users.length;
+  };
+
+  const renderTableBody = () => {
+    // let s = "";
+    return (
+      <tbody>
+        {users.map((u) => (
+          <tr key={u._id}>
+            <td>{u.name}</td>
+            <td>
+              {u.qualities.map((q) => (
+                <span key={u._id} className={"badge bg-" + q.color}>
+                  {q.name}
+                </span>
+              ))}
+            </td>
+            <td>{u.profession.name}</td>
+            <td>{u.completedMeetings}</td>
+            <td>{u.rate}</td>
+            <td>
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={() => handleDelete(u._id)}
+              >
+                delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
+
+  return (
+    <>
+      <span className="badge bg-info text-dark">{renderPhrase()}</span>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+
+        {renderTableBody()}
+      </table>
+    </>
+  );
 };
 
 export default Users;
