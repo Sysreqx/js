0b87f45517a21bede5012ff7c8e51eb27f664377
sysreqx <sysreqx@gmail.com>
2023-02-15 13:32:44 +0600

    feature(04): refactor

--- js_by_vladilen_react_crash_course/03_course_project/src/components/user.jsx
+++ js_by_vladilen_react_crash_course/03_course_project/src/components/user.jsx
@@ -1,71 +1,44 @@
-import React, { useState } from "react";
-import api from "../api";
-
-const Users = () => {
-  const [users, setUsers] = useState(api.users.fetchAll());
-  // console.log(users);
-
-  const handleDelete = (userId) => {
-    setUsers(users.filter((u) => u._id !== userId));
-    // console.log(userId);
-  };
-
-  const renderPhrase = (number) => {
-    if (users.length === 0) return "No users";
-    return users.length;
-  };
-
-  const renderTableBody = () => {
-    // let s = "";
+import React from "react";
+import Qualitie from "./qualitie";
+import BookMark from "./bookmark";
+const User = ({
+    _id,
+    name,
+    qualities,
+    profession,
+    completedMeetings,
+    rate,
+    onDelete,
+    bookmark,
+    onToggleBookMark,
+}) => {
     return (
-      <tbody>
-        {users.map((u) => (
-          <tr key={u._id}>
-            <td>{u.name}</td>
+        <tr>
+            <td>{name}</td>
             <td>
-              {u.qualities.map((q) => (
-                <span key={u._id} className={"badge bg-" + q.color}>
-                  {q.name}
-                </span>
-              ))}
+                {qualities.map((qual) => (
+                    <Qualitie key={qual._id} {...qual} />
+                ))}
             </td>
-            <td>{u.profession.name}</td>
-            <td>{u.completedMeetings}</td>
-            <td>{u.rate}</td>
+            <td>{profession.name}</td>
+            <td>{completedMeetings}</td>
+            <td>{rate} /5</td>
             <td>
-              <button
-                type="button"
-                className="btn btn-danger"
-                onClick={() => handleDelete(u._id)}
-              >
-                delete
-              </button>
+                <BookMark
+                    status={bookmark}
+                    onClick={() => onToggleBookMark(_id)}
+                />
             </td>
-          </tr>
-        ))}
-      </tbody>
+            <td>
+                <button
+                    onClick={() => onDelete(_id)}
+                    className="btn btn-danger"
+                >
+                    delete
+                </button>
+            </td>
+        </tr>
     );
-  };
-
-  return (
-    <>
-      <span className="badge bg-info text-dark">{renderPhrase()}</span>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-
-        {renderTableBody()}
-      </table>
-    </>
-  );
 };
 
-export default Users;
+export default User;
--- js_by_vladilen_react_crash_course/03_course_project/src/index.js
+++ js_by_vladilen_react_crash_course/03_course_project/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.css";
+import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Users from "./components/user";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Users />
-  </React.StrictMode>
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
