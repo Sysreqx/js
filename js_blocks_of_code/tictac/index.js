bff0a3d8facfeaa026ed66dbdc44df445dd9d141
sysreqx <sysreqx@gmail.com>
2023-02-21 12:22:27 +0600

    feature(tictac): pochti stable version

--- js_blocks_of_code/tictac/index.js
+++ js_blocks_of_code/tictac/index.js
@@ -1,16 +1,32 @@
 let playBackstage = document.querySelector("#play-backstage");
+let interviewBtn = document.querySelector("#interview-el");
 playBackstage.addEventListener("click", (event) => {
-    let {target} = event;
     showSecondScreen();
-})
+});
+
+interviewBtn.addEventListener("click", (event) => {
+    showInterviewScreen();
+});
 
 function changeVisibilityOnMobile(x) {
     let woman = document.querySelector("#woman");
     let mintOval = document.querySelector("#mint-oval");
 
+    let tickTackBox = document.querySelector("#tictac-box");
+
+    let sliderMobile = document.querySelector(".slider-mobile");
+
     if (x.matches) { // If media query matches
         woman.style.display = "none";
         mintOval.style.display = "none";
+
+        tickTackBox.style.width = "4.5em";
+        tickTackBox.style.top = "unset";
+        tickTackBox.style.bottom = "6em";
+        tickTackBox.style.right = "5.1em";
+
+        sliderMobile.style.display = "flex";
+
     } else {
         woman.style.display = "unset";
         mintOval.style.display = "unset";
@@ -18,7 +34,7 @@ function changeVisibilityOnMobile(x) {
 }
 
 let x = window.matchMedia("(max-width: 767px)")
-changeVisibilityOnMobile(x) // Call listener function at run time
+// changeVisibilityOnMobile(x) // Call listener function at run time
 // x.addListener(changeVisibilityOnMobile) // Attach listener function on state changes
 
 function showSecondScreen() {
@@ -29,14 +45,68 @@ function showSecondScreen() {
     let slider = document.querySelector("#snd-bg--slider");
     let sliderHeader = document.querySelector("#snd-bg--video-header");
 
+    let interviews = document.querySelector("#interviews");
+
     video.style.transform = "translateX(-100vw)";
     videoText1.style.transform = "translateX(-100vw)";
     videoText2.style.transform = "translateX(-100vw)";
 
+    interviews.style.transform = "translateX(-300vw)";
+
     slider.style.transform = "translateX(0)";
     sliderHeader.style.transform = "translateX(0)";
 
-    changeVisibilityOnMobile();
+    changeVisibilityOnMobile(x);
+}
+
+function changeVisibilityOnMobileInterview(x) {
+    let woman = document.querySelector("#woman");
+    let mintOval = document.querySelector("#mint-oval");
+
+    let tickTackBox = document.querySelector("#tictac-box");
+
+    let sliderMobile = document.querySelector(".slider-mobile");
+    let interviews = document.querySelector("#interviews");
+
+    if (x.matches) { // If media query matches
+        woman.style.display = "none";
+        mintOval.style.display = "none";
+
+        tickTackBox.style.width = "4.5em";
+        tickTackBox.style.top = "unset";
+        tickTackBox.style.bottom = "6em";
+        tickTackBox.style.right = "5.1em";
+
+        sliderMobile.style.display = "none";
+        interviews.style.transform = "translateX(-50%)";
+
+    } else {
+        woman.style.display = "unset";
+        mintOval.style.display = "unset";
+    }
+}
+
+function showInterviewScreen() {
+    let video = document.querySelector("#video-class");
+    let videoText1 = document.querySelector("#video-header1");
+    let videoText2 = document.querySelector("#video-header2");
+
+    let slider = document.querySelector("#snd-bg--slider");
+    let sliderHeader = document.querySelector("#snd-bg--video-header");
+
+    let interviews = document.querySelector("#interviews");
+
+    video.style.transform = "translateX(-100vw)";
+    videoText1.style.transform = "translateX(-100vw)";
+    videoText2.style.transform = "translateX(-100vw)";
+
+    slider.style.transform = "translateX(-200vw)";
+    sliderHeader.style.transform = "translateX(-200vw)";
+
+    // interviews.style.transform = "translateX(-50%)"; // mobile
+    interviews.style.transform = "translateX(0)"; // desk
+
+    changeVisibilityOnMobileInterview(x);
 }
 
 slideElements1();
@@ -201,3 +271,215 @@ function slideElements3() {
     let timeout = random(4000, 7000);
     setTimeout(slideElements3, timeout);
 }
+
+// Rythm sound button
+showMediaButtons();
+function showMediaButtons() {
+    let button1 = document.querySelector("#button1-mobile");
+    let buttons = document.querySelector("#buttons-mobile-inner");
+    button1.addEventListener("click", e => {
+        buttons.classList.toggle("display-none");
+        button1.classList.toggle("right-zero");
+        console.log("button1");
+    });
+
+    let button1Desktop = document.querySelector("#button1");
+    let button2 = document.querySelector("#button2");
+    let button3 = document.querySelector("#button3");
+    let button4 = document.querySelector("#button4");
+
+    button1Desktop.addEventListener("click", e => {
+        button2.classList.toggle("display-none");
+        button3.classList.toggle("display-none");
+        button4.classList.toggle("display-none");
+    });
+}
+
+// mobile slider
+const leftArrow = document.querySelector(".left-arrow"),
+    rightArrow = document.querySelector(".right-arrow"),
+    slider = document.querySelector(".slider-mobile--inner");
+
+/**
+ * @brief Scroll to the right
+ */
+function scrollRight() {
+    if (slider.scrollWidth - slider.clientWidth === slider.scrollLeft) {
+        slider.scrollTo({
+            left: 0,
+            behavior: "smooth"
+        });
+    } else {
+        slider.scrollBy({
+            left: window.innerWidth,
+            behavior: "smooth"
+        });
+    }
+}
+
+/**
+ * @brief Scroll to the left
+ */
+function scrollLeft() {
+    slider.scrollBy({
+        left: -window.innerWidth,
+        behavior: "smooth"
+    });
+}
+
+// Auto slider
+let timerId = setInterval(scrollRight, 7000);
+
+/**
+ * @brief Reset timer for scrolling right
+ */
+function resetTimer() {
+    clearInterval(timerId);
+    timerId = setInterval(scrollRight, 7000);
+}
+
+// Scroll Events
+leftArrow.addEventListener("click", function (ev) {
+        scrollLeft();
+        resetTimer();
+});
+
+rightArrow.addEventListener("click", function (ev) {
+        scrollRight();
+        resetTimer();
+});
+
+// slider interview
+// const sliderInterview = document.querySelector(".interviews");
+//
+// function scrollRightOnScrollY() {
+//     if (sliderInterview.scrollWidth - sliderInterview.clientWidth === sliderInterview.scrollLeft) {
+//         sliderInterview.scrollTo({
+//             left: 0,
+//             behavior: "smooth"
+//         });
+//     } else {
+//         sliderInterview.scrollBy({
+//             left: window.innerWidth,
+//             behavior: "smooth"
+//         });
+//     }
+// }
+//
+// /**
+//  * @brief Scroll to the left On scroll
+//  */
+// function scrollLeftOnScrollY() {
+//     sliderInterview.scrollBy({
+//         left: -window.innerWidth,
+//         behavior: "smooth"
+//     });
+// }
+
+
+// колесико мышки
+document.onwheel = wheelMeFunc;
+
+function wheelMeFunc(event) {
+    event.preventDefault();
+
+    // Up wheel
+    if (event.deltaY < 0) {
+        slideInterview();
+    }
+
+    // Down wheel
+    else {
+        slideInterview();
+    }
+}
+
+// A simple swipe detection on vanilla js
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const gestureZone = document.getElementById('first-screen-bg');
+
+gestureZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+gestureZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false);
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        // console.log('Swiped left');
+    }
+
+    if (touchendX > touchstartX) {
+        // console.log('Swiped right');
+    }
+
+    if (touchendY < touchstartY) {
+        // console.log('Swiped up');
+
+        slideInterview();
+    }
+
+    if (touchendY > touchstartY) {
+        // console.log('Swiped down');
+
+        slideInterview();
+    }
+
+    if (touchendY === touchstartY) {
+        // console.log('Tap');
+    }
+}
+// A simple swipe detection on vanilla js
+
+function slideInterview() {
+    let sliderInterview = document.querySelector("#interviews");
+    // console.log(sliderInterview);
+    let interview = sliderInterview.querySelectorAll(".interview");
+    // console.log(interview.length);
+
+    for (let i = 0; i < interview.length; i++) {
+
+        if (interview[i].classList.contains("interview-active")) {
+            console.log(`current ${i}`);
+            interview[i].classList.remove("interview-active"); // remove
+            interview[i].classList.add("interview-inactive"); // add
+            interview[i].style.zIndex = "91";
+            // interview[i].classList.add("slide-inactive-down"); // add
+
+            if (i >= interview.length - 1) {
+                interview[0].style.zIndex = "97";
+                interview[0].classList.add("interview-active"); // add
+                interview[0].classList.remove("interview-inactive"); //remove
+                console.log(`next 0`);
+
+                // setTimeout(removeInactiveDown1, 2000);
+
+                break;
+            }
+
+            interview[i + 1].style.zIndex = "97";
+            interview[i + 1].classList.add("interview-active"); // add
+            interview[i + 1].classList.remove("interview-inactive"); //remove
+            console.log(`next ${i + 1}`);
+
+
+            // setTimeout(removeInactiveDown1, 2000);
+
+            break;
+        }
+    }
+    // let timeout = random(4000, 7000);
+    // setTimeout(slideElements1, timeout);
+}
+
+
+
