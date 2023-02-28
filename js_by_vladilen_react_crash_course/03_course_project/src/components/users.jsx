d72c07ac06478d62c1fe6b75fe1c779b0009557c
sysreqx <sysreqx@gmail.com>
2023-02-28 13:12:38 +0600

    feature(react-learn): display pagination

--- js_by_vladilen_react_crash_course/03_course_project/src/components/users.jsx
+++ js_by_vladilen_react_crash_course/03_course_project/src/components/users.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 import User from "./user";
-const Users = ({ users, ...rest }) => {
+import Pagination from "./pagination";
+
+const Users = ({users, ...rest}) => {
+    const count = users.length;
+    const pageSize = 4;
+    const handlePageChange = (pageIndex) => {
+        console.log("page", pageIndex)
+    }
     return (
         <>
-            {users.length > 0 && (
+            {count > 0 && (
                 <table className="table">
                     <thead>
-                        <tr>
-                            <th scope="col">Имя</th>
-                            <th scope="col">Качества</th>
-                            <th scope="col">Провфессия</th>
-                            <th scope="col">Встретился, раз</th>
-                            <th scope="col">Оценка</th>
-                            <th scope="col">Избранное</th>
-                            <th />
-                        </tr>
+                    <tr>
+                        <th scope="col">Имя</th>
+                        <th scope="col">Качества</th>
+                        <th scope="col">Провфессия</th>
+                        <th scope="col">Встретился, раз</th>
+                        <th scope="col">Оценка</th>
+                        <th scope="col">Избранное</th>
+                        <th/>
+                    </tr>
                     </thead>
                     <tbody>
-                        {users.map((user) => (
-                            <User key={user._id} {...rest} {...user} />
-                        ))}
+                    {users.map((user) => (
+                        <User key={user._id} {...rest} {...user} />
+                    ))}
                     </tbody>
                 </table>
             )}
+            <Pagination itemsCount={count} pageSize={pageSize} onPageChange={handlePageChange}></Pagination>
+
         </>
     );
 };
