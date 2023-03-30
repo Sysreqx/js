2752615d86833c4583ab27a3d54778eb9082e1df
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-03-30 17:47:24 +0600

    feature(herbalife quiz): quiz logic

--- js_by_vladilen_react_crash_course/03_course_project/src/components/users.jsx
+++ js_by_vladilen_react_crash_course/03_course_project/src/components/users.jsx
@@ -2,11 +2,19 @@ import React, {useState} from "react";
 import User from "./user";
 import Pagination from "./pagination";
 import {paginate} from "../utils/paginate";
+import GroupList from "./groupList";
+import api from "../api";
 
-const Users = ({users, ...rest}) => {
+const Users = ({users: allUsers, ...rest}) => {
     const count = users.length;
     const pageSize = 4;
     const [currentPage, setCurrentPage] = useState(1);
+    const [professions, setProfession] = useState(api.profession.fetchAll());
+
+    const handleProfessionsSelect = (params) => {
+        console.log(params)
+    }
+
     const handlePageChange = (pageIndex) => {
         console.log("page", pageIndex);
         setCurrentPage(pageIndex);
@@ -16,13 +24,14 @@ const Users = ({users, ...rest}) => {
 
     return (
         <>
+            <GroupList items={professions} onItemSelect = {} />
             {count > 0 && (
                 <table className="table">
                     <thead>
                     <tr>
                         <th scope="col">Имя</th>
                         <th scope="col">Качества</th>
-                        <th scope="col">Провфессия</th>
+                        <th scope="col">Проффессия</th>
                         <th scope="col">Встретился, раз</th>
                         <th scope="col">Оценка</th>
                         <th scope="col">Избранное</th>
