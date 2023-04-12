a6c98b3cb60dc0ebcced06ad2851dad36b812fde
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-04-12 12:39:05 +0600

    feature(freedom broker): flip js

---
+++ js_blocks_of_code/freedom broker/index.js
@@ -0,0 +1,14 @@
+let buttons = document.querySelectorAll(".flip-box-button");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", (event) => {
+        let {target} = event;
+
+        // console.log(target);
+        if (target.classList.contains("elementor-heading-title")) {
+            target.parentElement.parentElement.parentElement.parentElement.classList.toggle("flip-box-inner-rotated");
+        } else if (target.classList.contains("elementor-widget-container")) {
+            target.parentElement.parentElement.parentElement.classList.toggle("flip-box-inner-rotated");
+        }
+    });
+}
