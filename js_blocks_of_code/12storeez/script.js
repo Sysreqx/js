c6d24dc5b4b22ae032d99d0af671ba43d654fa7a
Kaisar Yerdenbekov <sysreqx@gmail.com>
2024-01-15 14:23:44 +0600

    feature(...): ...

--- js_blocks_of_code/12storeez/script.js
+++ js_blocks_of_code/12storeez/script.js
@@ -8,6 +8,26 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-pagination',
         clickable: true,
     },
+    autoplay: {
+        delay: 5000,
+    },
     updateOnWindowResize: true,
     touchEventsTarget: 'container'
+});
+
+
+/*slider*/
+function slide1(){
+    let slideValue = document.getElementById("slider-1").value;
+    document.getElementById("my-img-1").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+}
+
+function slide2(){
+    let slideValue = document.getElementById("slider-2").value;
+    document.getElementById("my-img-2").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+}
+
+let list = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
+list.forEach(function(element) {
+    element.setAttribute('href', 'https://harpersbazaar.kz/wp-content/uploads/2018/12/favicon.ico');
 });
